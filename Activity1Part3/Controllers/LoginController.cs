0a824a0ba04ad8deea4ea67859033b9fb61a62f0
Isaac Tucker <isaac.takka@gmail.com>
2020-12-08 18:32:25 -0700

    Commiting activity 4
    activity 4

--- Activity1Part3/Controllers/LoginController.cs
+++ Activity1Part3/Controllers/LoginController.cs
@@ -1,15 +1,19 @@
 ﻿using Activity1Part3.Models;
 using Activity1Part3.Services.Business;
+using Activity1Part3.Services.Utility;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Activity1Part3.Controllers
 {
     public class LoginController : Controller
     {
+        private static MyLogger logger = MyLogger.GetInstance();
         // GET: Login
         [HttpGet]
         public ActionResult Index()
@@ -20,23 +24,37 @@ namespace Activity1Part3.Controllers
         [HttpPost]
         public ActionResult Login(UserModel model)
         {
-            if (ModelState.IsValid)
-            {
+            logger.Info("Entering LoginController.Login()");
 
-                SecurityService sservice = new SecurityService();
-                bool results = sservice.Authenticate(model);
-                if (results)
+            try
+            {
+                if (ModelState.IsValid)
                 {
-                    return View("LoginPassed", model);
+
+                    SecurityService sservice = new SecurityService();
+                    bool results = sservice.Authenticate(model);
+                    if (results)
+                    {
+                        logger.Info("Parameters are: " + new JavaScriptSerializer().Serialize(model));
+                        logger.Info("Exit LoginControlle.DoLogin() with login passing");
+                        return View("LoginPassed", model);
+                    }
+                    else
+                    {
+                        logger.Info("Parameters are: " + new JavaScriptSerializer().Serialize(model));
+                        logger.Info("Exit LoginController.DoLogin() with login failing");
+                        return View("LoginFailed");
+                    }
                 }
                 else
                 {
-                    return View("LoginFailed");
+                    return View(model);
                 }
-            } 
-            else
+            }
+            catch (Exception e)
             {
-                return View(model);
+                logger.Error("Exception LoginController.DoLogin()" + e.Message);
+                return Content("Exception in login" + e.Message);
             }
         }
 
